558880c4fa61dc21dd35f59cee3cbcbfdfdf6d61
Edwin Djonorh <edjonorh@gmail.com>
2022-02-25 00:23:51 +0000

    setted up a test suit template

--- tests/test.py
+++ tests/test.py
@@ -6,3 +6,12 @@ from dotenv import load_dotenv
 from main import getEnv
 from pathlib import Path
 load_dotenv(dotenv_path = Path('.env'))
+
+class TestExample(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
